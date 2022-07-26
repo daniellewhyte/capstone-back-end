385e71d9e18389b5879db96cd864f381cee165de
Danielle <dwhyte1226@gmail.com>
2022-07-26 16:48:01 -0500

    created viewset and serializer

--- parsely/parselyBackEnd/views.py
+++ parsely/parselyBackEnd/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from parselyBackEnd.models import Recipe
+from parselyBackEnd.serializers import RecipeSerializer
+from rest_framework import viewsets
 
 # Create your views here.
+class RecipeViewSet(viewsets.ModelViewSet):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
