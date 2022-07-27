6e0dc3e12da0ca2cb6dc056a67d21991590405c6
Danielle <dwhyte1226@gmail.com>
2022-07-27 15:41:45 -0500

    added properties to model to hold recipe data

--- parsely/parselyBackEnd/models.py
+++ parsely/parselyBackEnd/models.py
@@ -1,11 +1,26 @@
 from django.db import models
+from parselyBackEnd.helper import parse_text
 
 # Create your models here.
 
 class Recipe(models.Model):
-    title = models.CharField(max_length=250)
-    ingredients = models.TextField()
-    instructions = models.TextField()
+    url = models.TextField(null=False)
 
     def __str__(self):
         return self.title
+
+    @property
+    def recipe(self):
+        return parse_text(self.url)
+
+    @property
+    def title(self):
+        return self.recipe["title"]
+
+    @property
+    def ingredients(self):
+        return self.recipe["ingredients"]
+
+    @property
+    def instructions(self):
+        return self.recipe["instructions"]
