0fe96093b2ce7d0b3bd1f256866bd97a79e7fce1
Danielle <dwhyte1226@gmail.com>
2022-07-26 20:51:16 -0500

    trying to override create method in serializer. Unsuccessful

--- parsely/parselyBackEnd/serializers.py
+++ parsely/parselyBackEnd/serializers.py
@@ -5,3 +5,8 @@ class RecipeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Recipe
         fields = ("title", "ingredients", "instructions")
+
+    def create(self, recipe_data):
+        return Recipe(title=recipe_data["title"], 
+                        ingredients=recipe_data["ingredients"], 
+                        instructions=recipe_data["instructions"])
