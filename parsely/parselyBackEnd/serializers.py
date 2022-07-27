e981871feaa10d109c4836cb3b28b754a05df9d3
Danielle <dwhyte1226@gmail.com>
2022-07-27 15:44:38 -0500

    updated serializer

--- parsely/parselyBackEnd/serializers.py
+++ parsely/parselyBackEnd/serializers.py
@@ -2,11 +2,11 @@ from rest_framework_json_api import serializers
 from parselyBackEnd.models import Recipe
 
 class RecipeSerializer(serializers.HyperlinkedModelSerializer):
+
+    title = serializers.ReadOnlyField()
+    ingredients = serializers.ReadOnlyField()
+    instructions = serializers.ReadOnlyField()
+
     class Meta:
         model = Recipe
-        fields = ("title", "ingredients", "instructions")
-
-    def create(self, recipe_data):
-        return Recipe(title=recipe_data["title"], 
-                        ingredients=recipe_data["ingredients"], 
-                        instructions=recipe_data["instructions"])
+        fields = ["url", "title", "ingredients", "instructions"]
