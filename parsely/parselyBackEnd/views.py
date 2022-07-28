152d1567b8906be91037bc35763d8fe817cf0fd1
Danielle <dwhyte1226@gmail.com>
2022-07-27 22:01:07 -0500

    created new viewset for titles

--- parsely/parselyBackEnd/views.py
+++ parsely/parselyBackEnd/views.py
@@ -1,8 +1,12 @@
 from parselyBackEnd.models import Recipe
-from parselyBackEnd.serializers import RecipeSerializer
+from parselyBackEnd.serializers import RecipeSerializer, TitleSerializer
 from rest_framework import viewsets
 
 # Create your views here.
 class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
+
+class TitleViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Recipe.objects.all()
+    serializer_class = TitleSerializer
