0894c9040e4a489140dfb7fd36ffaf16926761aa
Danielle <dwhyte1226@gmail.com>
2022-07-28 19:40:19 -0500

    add heroku app to allowed hosts

--- parsely/settings.py
+++ parsely/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-i%m20ds2cets#2geldn2ng_!&&f%qzfsuhluj8&h1qvpx327i0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['parsely-backend.herokuapp.com']
 
 
 # Application definition
