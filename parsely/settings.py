fb6e8e2dd64defb1c8ec09e494293035d16e137a
Danielle <dwhyte1226@gmail.com>
2022-07-29 11:22:38 -0500

    installed django cors headers

--- parsely/settings.py
+++ parsely/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ['parsely-backend.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'psycopg2',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -47,6 +48,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -164,3 +166,8 @@ REST_FRAMEWORK = {
     ),
     'TEST_REQUEST_DEFAULT_FORMAT': 'vnd.api+json'
 }
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000/',
+    'chrome-extension://hpofgdemiocplnioceoeabeijklhmama',
+]
