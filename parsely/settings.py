11ac3be29050b33cfc2cabd64c8c6513e3f8b25d
Danielle <dwhyte1226@gmail.com>
2022-08-15 23:01:03 -0500

    add front end to allowed origins

--- parsely/settings.py
+++ parsely/settings.py
@@ -172,4 +172,5 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'chrome-extension://hpofgdemiocplnioceoeabeijklhmama',
+    'https://parsely-web.herokuapp.com',
 ]
