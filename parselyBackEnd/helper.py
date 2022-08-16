42ad42672a6f8d7dc04b916720eb1a43e7fe7144
Danielle <dwhyte1226@gmail.com>
2022-08-15 20:27:36 -0500

    updated function to potentially work with other sites

--- parselyBackEnd/helper.py
+++ parselyBackEnd/helper.py
@@ -1,19 +1,18 @@
 import requests
 from bs4 import  BeautifulSoup
-
-TEST_URL = "https://sallysbakingaddiction.com/mini-powdered-sugar-donut-muffins/"
+import re
 
 def parse_text(url):
-    page = requests.get(url)
-    soup = BeautifulSoup(page.text, 'html5lib')
-    title = soup.title.get_text()
-    search_ingredients = soup.find(class_="tasty-recipes-ingredients")
-    search_instructions = soup.find(class_="tasty-recipes-instructions")
-    ingredients = search_ingredients.prettify()
-    instructions = search_instructions.prettify()
-    recipe = {
-            "title": title,
-            "ingredients": ingredients,
-            "instructions": instructions
-            }
-    return recipe
+        page = requests.get(url)
+        soup = BeautifulSoup(page.text, 'html5lib')
+        title = soup.title.get_text()
+        search_ingredients = soup.find(class_= re.compile(".*ingredients.*"))
+        search_instructions = soup.find(class_=re.compile(".*instructions.*"))
+        ingredients = search_ingredients.prettify()
+        instructions = search_instructions.prettify()
+        recipe = {
+                "title": title,
+                "ingredients": ingredients,
+                "instructions": instructions
+                }
+        return recipe
